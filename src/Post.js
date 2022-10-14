9fe8b98593629b570ca0244a7a59280519b7fb04
Benwaz78 <nonwaz78@gmail.com>
2022-10-14 14:07:36 +0100

    End of basic react

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+
+function Post(){
+    return(
+        <div className="post">
+            <h1>Post 1</h1>
+            <p>
+                I love my paragraph here and always
+            </p>
+        </div>
+    )
+}
+
+export default Post;
